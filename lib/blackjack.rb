1d02c5d4f54e904d4483679045501ef189e667f8
devchea <rchea817@gmail.com>
2020-04-18 10:17:11 -0700

    update

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,71 @@
+require'pry'
+
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  input = gets.chomp
+  input
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  hand_total = deal_card + deal_card
+  display_card_total (hand_total)
+  return hand_total
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
+end
+
+# def hit?
+  # code hit? here
+  # prompt_user
+  # binding.pry
+  # user_input = get_user_input
+  # while get_user_input != 
+# end
+
+def hit?(current_total)
+  prompt_user
+  return current_total
+  user_input = get_user_input
+  while user_input != "h" && user_input != "s"
+    case user_input
+      when "h"
+        current_total += deal_card
+      when "s"
+        return current_total
+      else
+      invalid_command
+    end
+  
+  end
 end
 
 #####################################################
@@ -41,4 +75,3 @@ end
 def runner
   # code runner here
 end
-    
